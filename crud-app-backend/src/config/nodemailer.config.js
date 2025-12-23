02a05cee8fdd527026341b52b2628f9225a283c2
Ajay Kumar <rohitkainwal@gmail.com>
2025-12-23 13:54:45 +0530

    `Updated nodemailer configuration and email sending functionality in various routes and utilities`

--- crud-app-backend/src/config/nodemailer.config.js
+++ crud-app-backend/src/config/nodemailer.config.js
@@ -1,18 +1,18 @@
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config({quiet:true});
+dotenv.config({quiet:true});
 
-// import nodemailer from "nodemailer"
+import nodemailer from "nodemailer"
 
-// const mailTransport = nodemailer.createTransport({
-//      host: process.env.NODEMAILER_HOST,
-//   port: process.env.NODEMAILER_PORT,
-//   secure: process.env.NODEMAILER_SECURE,
-//   service: process.env.NODEMAILER_SERVICE,
-//   auth: {
-//     user: process.env.NODEMAILER_EMAIL,
-//     pass: process.env.NODEMAILER_PASSWORD,
-//   },
-// })
+const mailTransport = nodemailer.createTransport({
+     host: process.env.NODEMAILER_HOST,
+  port: process.env.NODEMAILER_PORT,
+  secure: process.env.NODEMAILER_SECURE,
+  service: process.env.NODEMAILER_SERVICE,
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASSWORD,
+  },
+})
 
-// export default mailTransport;
+export default mailTransport;
